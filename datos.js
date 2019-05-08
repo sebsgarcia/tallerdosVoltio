a6af668e87ff40c0406506cd7ed108aa4b2d2049
Sebastian <sebas971129@gmail.com>
2019-05-07 21:42:03 -0500

    12 archivos de productos en el database

--- datos.js
+++ datos.js
@@ -1,11 +1,140 @@
 module.exports = [
 {
-    name: "Night Blouse",
-    
-    img: "",
-    type: "Shirt",
+    name: "Night Blouse", 
+    img: "/imagenes/item1.jpg",
+    type: "blouse",
     price: 20.95,
     size:"M"
+},
+{
+    name: "Lakers T", 
+    img: "/imagenes/kids1.jpg",
+    type: "t-Shirt",
+    price: 15.99,
+    size:"M"
+},
+{
+    name: "80s Jacket", 
+    img: "/imagenes/man1.jpg",
+    type: "jacket",
+    price: 15.99,
+    size:"M"
+},
+//new
+{
+    name:"Pink Big Tee",
+    imagenes:"/imagenes/mujeres/tops/item1/item1.jpg",
+    category:"womens",
+    desc:"Made from a super soft, semi-sheer burnout knit in a menswear fit, The Big Brother Tee is our new favorite crew-neck. Find it only at UO.",
+    type:"top",
+    price:"20.99",
+    size:"S"
 }
+,
+{
+    name:"Flower Chart",
+    imagenes:"/imagenes/mujeres/tops/item3/item1.jpg",
+    category:"womens",
+    desc:"In full bloom with this tee from Future State. Made from a super soft cotton in a classic-fit construction. Topped with a banded crew-neck + short sleeves. Finished with graphics at the front.",
+    type:"top",
+    price:"34.00",
+    size:"S"
+}
+,
+{
+    name:"Levi’s Vintage Denim Shortall Overall t",
+    imagenes:"/imagenes/mujeres/bottoms/item1/item1.jpg",
+    category:"womens",
+    desc:"Vintage-inspired denim shortall overall from iconic label, Levi’s. Trimmed with adjustable, thick racerback straps that clasp at the apron chest with a pouch pocket accent. Low, open sides fitted with button closures at the hips, 4-pocket design + finished with rolled cuffs for a vintage look we love.",
+    type:"bottoms",
+    price:"44.00",
+    size:"S"
+}
+,
+{
+    name:"Arlo Pleated Pant",
+    imagenes:"/imagenes/mujeres/bottoms/item2/item1.jpg",
+    category:"womens",
+    desc:"Menswear-inspired pleated pant from Urban Outfitters. Made from a woven fabrication in a high-rise construction that’s relaxed-fit all the way through the lightly tapered leg",
+    type:"bottoms",
+    price:"54.00",
+    size:"S"
+}
+,
+//3
+{
+    name:"Darren Canvas Overall",
+    imagenes:"/imagenes/mujeres/bottoms/item3/item1.jpg",
+    category:"womens",
+    desc:"Canvas overall with classic button detailing from BDG. Built from a thick canvas that’s relaxed through the straight leg. Cut with low open sides and adjustable thick racerback straps that clasp at the apron chest. ",
+    type:"bottoms",
+    price:"99.00",
+    size:"S"
+},
+{
+    name:"Babydoll Mini Dress",
+    imagenes:"/imagenes/mujeres/dresses/item1/item1.jpg",
+    category:"womens",
+    desc:"Swingy mini dress from Urban Outfitters cut in relaxed, frock-style silhouette. Lightweight cotton cut with tiered ruffle detailing + button closure with keyhole cutout at the back.",
+    type:"dresses",
+    price:"49.00",
+    size:"S"
+}
+,
+{
+    name:"La Petite Tie-Back Babydoll Dress",
+    imagenes:"/imagenes/mujeres/dresses/item2/item1.jpg",
+    category:"womens",
+    desc:"Sweetly styled mini dress from Urban Outfitters. Lightweight cotton cut in a babydoll silhouette with exaggerated ruffle cap sleeves + tie closure at the open back.",
+    type:"dresses",
+    price:"59.00",
+    size:"S"
+}
+,
+{
+    name:"Surplice Midi Slip Dress",
+    imagenes:"/imagenes/mujeres/dresses/item3/item1.jpg",
+    category:"womens",
+    desc:"Slip into this effortlessly styled midi dress, only at Urban Outfitters. Soft, flowing fabric cut in a slim fit with a surplice neckline + slit accent at the swingy, midi-length hem. Finished with adjustable spaghetti straps + zipper closure at the side.",
+    type:"dresses",
+    price:"35.00",
+    size:"S"
+}
+//4
+,
+{
+    name:"Levi’s Baggy Denim Sherpa Trucker Jacket",
+    imagenes:"/imagenes/mujeres/jackets/item1/item1.jpg",
+    category:"womens",
+    desc:"Essential Levi's trucker jacket gets a slouchy new look that makes layering a dream. Made from a vintage-look denim that’s lined with soft + cozy sherpa to keep you warm. ",
+    type:"jackets",
+    price:"125.00",
+    size:"S"
+},
+{
+    name:"Vintage 1950s Oversized Work Jacket",
+    imagenes:"/imagenes/mujeres/jackets/item2/item1.jpg",
+    category:"womens",
+    desc:"Our Urban Renewal experts discovered this stash of vintage oversized workwear jackets. Cut from a vintage cotton twill in a super relaxed fit with a button-down placket. Topped with a pointed collar + long sleeves that feature button cuffs. ",
+    type:"jackets",
+    price:"59.00",
+    size:"S"
+},
+{
+    name:"Vintage 1950s Oversized Work Jacket",
+    imagenes:"/imagenes/mujeres/jackets/item3/item1.jpg",
+    category:"womens",
+    desc:"Our Urban Renewal experts discovered this stash of vintage oversized workwear jackets. Cut from a vintage cotton twill in a super relaxed fit with a button-down placket.",
+    type:"jackets",
+    price:"59.00",
+    size:"S"
+}
+
+
+
+
+
+
+
 
 ]
--- index.js
+++ index.js
@@ -2,25 +2,74 @@ const express = require('express');
 const hbs = require('express-handlebars');
 const app = express();
 
+
+//instalar Mongo
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+
+
+//Conection URL
+const url = 'mongodb://localhost:27017';
+const dbName = 'voltio';
+
+//Create Clietn Object
+const client = new MongoClient(url, { useNewUrlParser: true });
+
+
+var db = null;
+
+
+
+//connect 
+client.connect(function (err) {
+    assert.equal(null, err);
+    console.log("Conecto a la base de datos");
+
+    //Conect to database
+     db = client.db(dbName);
+
+//    client.close();
+
+});
+
 //Definir la carpeta public
 app.use(express.static('public'));
 //para registrar el motor de render de handlebars
-app.engine('handlebars',hbs());
+app.engine('handlebars', hbs());
 //para setear el motor de render a utilizar
-app.set('view engine','handlebars');
+app.set('view engine', 'handlebars');
 
-var shirts  = require('./datos');
+var shirts = require('./productos');
+
+//app.use(express.static(path.join(__dirname, 'static')));
 //definir ruta root o principal
-app.get('/', function(request, response){
+
+app.get('/', function (request, response) {
 
     var contexto = {
         titulo: 'titulo',
         productos: shirts
     }
-    
+
     response.render('home', contexto);
 });
 
-app.listen(4000, function(){
+app.get('/woman', function (request, response) {
+
+    var productos = db.collection('productos');
+    productos.find({}).toArray(function (err, docs) {
+        assert.equal(null, err);
+
+         var contexto = {
+             productos: docs
+         
+         };
+         response.render('productoswoman', contexto);
+    }); 
+
+});
+
+app.listen(3000, function () {
     console.log('Cargo');
 });
